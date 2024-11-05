3486859d7d55de431f624b24c2e205cec5b04d24
mrrustemka <mubarakshin1996@gmail.com>
2024-11-05 21:44:24 +0400

    implement-slogan

---
+++ src/Components/Slogan.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Slogan() {
+  return (
+    <div className="slogan">
+      Забирай бонус и <br />
+      срывай джекпот
+    </div>
+  );
+}
+
+export default Slogan;
